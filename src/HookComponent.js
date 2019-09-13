d917230a5c2b2c49c56f8c895795e718bd3d2898
ken8waru1 <ken8waru1@gmail.com>
2019-09-13 14:23:10 -0700

    Complete conversion of class component to hook component

--- src/HookComponent.js
+++ src/HookComponent.js
@@ -2,14 +2,46 @@ import React, { useState, useEffect } from 'react';
 
 function HookComponent(props) {
   const [name, setName] = useState('');
-  const restricted = ['voldemort', 'moriarty', 'maleficent', 'palpatine'];
+  //keep restricted words in a set for constant lookup time
+  const restricted = new Set(['voldemort', 'moriarty', 'maleficent', 'palpatine']);
+
+  useEffect(() => {
+    handleTitle();
+    handleColor();
+  })
+
+  const handleInput = e => {
+    setName(e.currentTarget.value);
+  }
+
+  const isRestricted = () => {
+    if (restricted.has(name.toLocaleLowerCase())) return true;
+  }
+
+  const handleTitle = () => {
+    if (!name) {
+      document.title = 'Name Check';
+    } else if (isRestricted()) {
+      document.title = '禁止禁止禁止禁止';
+    } else {
+      document.title = name;
+    }
+  }
+
+  const handleColor = () => {
+    if (isRestricted()) {
+      document.getElementsByClassName('form')[0].style.backgroundColor = 'red';
+    } else {
+      document.getElementsByClassName('form')[0].style.backgroundColor = 'white';
+    }
+  }
 
   return (
     <section className='form'>
-      <header>Checking: </header>
+      <header>Checking: {name}</header>
       <label>Please provide your name
           <input
-          value='something'
+          value={name}
           onChange={handleInput}
         />
       </label>
